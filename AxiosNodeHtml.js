c89a4ee353e0e7cc26d899d0fcbe0287f2e6165c
Wanidx <s6506021610045@kmutnb.ac.th>
2024-02-06 10:18:27 +0700

    main

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(__dirname + '/public'));
 
-app.get("/", asyn (req, res) => {
+app.get("/", async (req, res) => {
     try {
         const response = await axios.get(base_url + '/books');
         res.render("books", {books: response.data});
@@ -23,6 +23,61 @@ app.get("/", asyn (req, res) => {
 
 app.get("/book/:id", async (req, res) => {
     try {
-        const response = await axios.get(base)
+        const response = await axios.get(base_url + '/books/' + req.params.id);
+        res.render("book", {book: response.data});
+    } catch(err) {
+        console.error(err);
+        res.status(500).send('Error');
     }
+});
+
+app.get("/create", (req, res) => {
+    res.render("create");
+});
+
+app.post("/create", async (req, res) => {
+    try {
+        const data = {title: req.body.title, author: req.body.author};
+        await axios.post(base_url + '/books', data);
+        res.redirect("/");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
+
+app.get("/update/:id", async (req, res) => {
+    try {
+        const response = await axios.get(
+        base_url + '/books/' + req.params.id);
+        res.render("update", {book: response.data});  
+    } catch(err) {
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
+
+app.post("/update/:id", async (req, res) => {
+    try {
+        const data = {title: req.body.title, author: req.body.author};
+        await axios.put(base_url + '/books/' + req.params.id, data);
+        res.redirect("/");
+    } catch(err) {
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
+
+app.get("/delete/:id", async (req, res) => {
+    try {
+        await axios.delete(base_url + '/books/' + req.params.id);
+        res.redirect("/");
+    } catch(err) {
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
+
+app.listen(5500, () => {
+    console.log('Server stated on port 5500');
 });
