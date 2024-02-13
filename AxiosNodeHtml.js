e8ea5db4518996f472835b57f4882d7d37d654e3
Wanidx <s6506021610045@kmutnb.ac.th>
2024-02-13 09:37:30 +0700

    main

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 // const base_url = "http://localhost:3000";
 const base_url = "http://node56765-wanichanon.proen.app.ruk-com.cloud";
 
+app.set("views", path.join(__dirname, "/public/views"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
